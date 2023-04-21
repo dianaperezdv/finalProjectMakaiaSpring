b9e3a399e727ac5c6d073f3d9f25c3b8f840a5b3
Diana Camila Pérez <dianaperezdv@gmail.com>
2023-04-21 02:57:54 -0500

    Revert "Intento"
    This reverts commit 6de6d85b67b436e9e5163bde67cc7002b591cd22.

---
+++ src/test/java/com/example/finalProject/Services/PaqueteServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.finalProject.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaqueteServiceTest {
+
+    @Test
+    void crearPaquete() {
+    }
+
+    @Test
+    void calcularTipo() {
+    }
+}
---
+++ src/test/java/com/example/finalProject/Services/Tests.java
@@ -0,0 +1,17 @@
+package com.example.finalProject.Services;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    //Create a test for the method "crearCliente" in ClienteService.java
+    //The test should verify that a client is created correctly
+    //The test should verify that a client is not created when the cedula is invalid
+    //The test should verify that a client is not created when the cedula already exists
+    //The test should verify that a client is not created when the cedula is invalid and already exists
+    //The test should verify that a client is not created when the cedula is valid and does not exist
+    @Test
+    void crearCliente() {
+
+
+    }
+}
